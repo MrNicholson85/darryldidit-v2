34e1afd5c5a285e4b2a44a5e02aa7d3a64339243
Darryl Nicholson <darryl.a.nicholson@gmail.com>
2018-09-15 12:01:55 -0700

    added COT projects

---
+++ wp-content/themes/ddi-v2/resources/views/archive-projects.blade.php
@@ -0,0 +1,23 @@
+{{--
+  Template Name: Project Template
+--}}
+
+
+@extends('layouts.app')
+
+@section('content')
+  @include('partials.page-header')
+
+  @if (!have_posts())
+    <div class="alert alert-warning">
+      {{ __('Sorry, no results were found.', 'sage') }}
+    </div>
+    {!! get_search_form(false) !!}
+  @endif
+
+  @while (have_posts()) @php the_post() @endphp
+    @include('partials.content-'.get_post_type())
+  @endwhile
+
+  {!! get_the_posts_navigation() !!}
+@endsection
