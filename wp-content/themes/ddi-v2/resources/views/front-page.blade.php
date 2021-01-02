47f51aca3182e405a467d969b9af14a83c0204ec
Darryl Nicholson <darryl.a.nicholson@gmail.com>
2021-01-02 13:10:24 -0800

    Added classic editor, sterted on Hero module

--- wp-content/themes/ddi-v2/resources/views/front-page.blade.php
+++ wp-content/themes/ddi-v2/resources/views/front-page.blade.php
@@ -6,6 +6,8 @@
 
 @section('content')
   @while(have_posts()) @php the_post() @endphp
-    @include('partials.page-header')
+    @include('partials.hero')
   @endwhile
+
+  <div class="">Hello Darryl !</div>
 @endsection
--- wp-content/themes/ddi-v2/resources/views/partials/page-header.blade.php
+++ wp-content/themes/ddi-v2/resources/views/partials/page-header.blade.php
@@ -1,3 +1,9 @@
 <div class="page-header">
   <h1>{!! App::title() !!}</h1>
+  <div class="continer">
+    <div class="row">
+      <div class="col-md-4"> Logo </div>
+      <div class="col-md-8"> Nav </div>
+    </div>
+  </div>
 </div>
