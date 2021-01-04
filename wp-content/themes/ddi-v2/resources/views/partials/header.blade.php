e9054c27f922bef57455913fc3dd43ed949e5200
Darryl Nicholson <darryl.a.nicholson@gmail.com>
2021-01-04 12:50:40 -0800

    hero wip
    updated the font on diff breakpoints
    added some site colors

--- wp-content/themes/ddi-v2/resources/views/partials/header.blade.php
+++ wp-content/themes/ddi-v2/resources/views/partials/header.blade.php
@@ -1,5 +1,5 @@
 
-<header class="banner homepage-hero">
+<header class="banner navbar fixed-top">
   <div class="container">
     <nav class="nav-primary">
       @if (has_nav_menu('primary_navigation'))
--- wp-content/themes/ddi-v2/resources/views/partials/hero.blade.php
+++ wp-content/themes/ddi-v2/resources/views/partials/hero.blade.php
@@ -1,16 +1,18 @@
 <div class="hero"  style="background-image: url('{{$hero_image}}');">
   <div class="container">
     <div class="row">
-      <div class="col-md-12">
-        <div class="hero__title">
-          @if ($hero_title)
-            {{ $hero_title }}        
-          @endif
-        </div>
-        <div class="hero__sub-title">
-          @if ($sub_title)
-            {{ $sub_title }}        
-          @endif
+      <div class="hero__content">
+        <div class="col-md-12">
+          <div class="hero__title h1">
+            @if ($hero_title)
+              {{ $hero_title }}        
+            @endif
+          </div>
+          <div class="hero__sub-title h2">
+            @if ($sub_title)
+              {{ $sub_title }}        
+            @endif
+          </div>
         </div>
       </div>
     </div>
