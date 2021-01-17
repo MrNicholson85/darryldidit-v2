f810d30abcad44dc383e6c186064a583c80fb552
Darryl Nicholson <darryl.a.nicholson@gmail.com>
2021-01-16 22:51:57 -0800

    added styles to single project (wip)

--- wp-content/themes/ddi-v2/resources/views/partials/content-single-project.blade.php
+++ wp-content/themes/ddi-v2/resources/views/partials/content-single-project.blade.php
@@ -1,27 +1,32 @@
-@debug
-@if($project_loop)
-
-    <div class="page-section single-project">
-        <div class="container-fluid" style="background-image: url({{ $single_project->project_image['url'] }})">
-            {{ $single_project->project_title }}
+@if($single_project)
+<div class="single-project">
+  <div class="container-fluid single-project__project-hero" style="background-image: url({{ $single_project->project_image['url'] }})">
+    <div class="container">
+      <div class="row">
+        <div class="single-project__hero-title">
+          {{ $single_project->project_title }}
         </div>
-        <div class="container">
-            <div class="row">
-                <div class="col-md-8">
-                    <img src=" {{ $single_project->project_image['url'] }} " class="img-fluid" />
-                </div>
-                <div class="col-md-4">
-                    <div class=""> {{ $single_project->project_content }}</div>
-                    <div class="">
-                    <ul>
-                        @foreach($single_project->project_skills as $skills)
-                            <li>{{ $skills['skills_used'] }}</li>
-                        @endforeach
-                    </ul>
-                    </div>
-                </div>
-            </div>
-        </div>
+      </div>
     </div>
-
+  </div>
+  <div class="page-section">
+    <div class="container">
+      <div class="row">
+        <div class="col-md-8">
+          <div class="single-project__feat-image">
+            <img src="{{ $single_project->project_image['url'] }}" class="img-fluid" />
+          </div>
+        </div>
+        <div class="col-md-4">
+          <div class="single-project__content"> {{ $single_project->project_content }}</div>
+            <ul class="single-project__skill-list">
+              @foreach($single_project->project_skills as $skills)
+                <li class="single-project__skill-item">{{ $skills['skills_used'] }}</li>
+              @endforeach
+            </ul>
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
 @endif
