7eba26113137fc64f67f1eaa653f8323870180e5
Darryl Nicholson <darryl.a.nicholson@gmail.com>
2021-02-05 13:40:13 -0800

    added theme option page

---
+++ wp-content/themes/ddi-v2/app/fields/theme-option.php
@@ -0,0 +1,115 @@
+<?php
+
+use StoutLogic\AcfBuilder\FieldsBuilder;
+
+acf_add_options_page([
+    'page_title' => get_bloginfo('name') . ' theme options',
+    'menu_title' => 'Theme Options',
+    'menu_slug'  => 'theme-options',
+    'capability' => 'edit_theme_options',
+    'position'   => '999',
+    'autoload'   => true
+]);
+
+$options = new FieldsBuilder('theme_options');
+
+$options
+    ->setLocation('options_page', '==', 'theme-options');
+
+$options
+->addTab('contact_info', ['placement' => 'left'])
+    ->addText('Contact Title', [
+        'label' => 'Contact Title',
+    ])
+    ->addTextarea('Contact Message', [
+        'label' => 'Contact Message'
+    ])
+
+    ->addGroup('phone_info', [
+        'label' => 'Phone info',
+        'wrapper' => [
+            'width' => '33%'
+        ]
+    ])
+        ->addSelect('icon_select', [
+            'label' => 'Icon',
+        ])
+        ->addChoices([
+            'phone' => 'Phone',
+            'mail' => 'Mail',
+            'location' => 'Location',
+        ])
+        ->addText('pre_title', [
+            'label' => 'Pre Title'
+        ])
+        ->addText('phone_number', [
+            'label' => 'Phone Number'
+        ]) 
+    ->endGroup()
+    
+    ->addGroup('email_info', [
+        'label' => 'Email info',
+        'wrapper' => [
+            'width' => '34%'
+        ]
+    ])
+        ->addSelect('icon_select', [
+            'label' => 'Icon',
+        ])
+        ->addChoices([
+            'phone' => 'Phone',
+            'mail' => 'Mail',
+            'location' => 'Location',
+        ])
+        ->addText('pre_title', [
+            'label' => 'Pre Title'
+        ])
+        ->addText('phone_number', [
+            'label' => 'Phone Number'
+        ]) 
+    ->endGroup()
+    
+    ->addGroup('location_info', [
+        'label' => 'Location info',
+        'wrapper' => [
+            'width' => '33%'
+        ]
+    ])
+        ->addSelect('icon_select', [
+            'label' => 'Icon',
+        ])
+        ->addChoices([
+            'phone' => 'Phone',
+            'mail' => 'Mail',
+            'location' => 'Location',
+        ])
+        ->addText('pre_title', [
+            'label' => 'Pre Title'
+        ])
+        ->addText('phone_number', [
+            'label' => 'Phone Number'
+        ]) 
+    ->endGroup()
+    
+    
+    ->addGroup('contact_form_info', [
+        'label' => 'Form info',
+        'wrapper' => [
+            'width' => '50%'
+        ]
+    ])
+        ->addWYSIWYG('contact_form') 
+    ->endGroup()
+    
+    ->addGroup('map_info', [
+        'wrapper' => [
+            'width' => '50%'
+        ]
+    ])
+        ->addGoogleMap('map', [
+            'label' => 'Map',
+        ])
+    ->endGroup();
+    
+
+return $options;
