87eab3869f3e1591d1a318295d6c2079270fe17a
Darryl <darryl.a.nicholson@gmail.com>
2021-09-03 12:56:23 -0700

    checkpoint

--- wp-content/themes/ddi-v2/resources/views/archive-projects.blade.php
+++ wp-content/themes/ddi-v2/resources/views/archive-projects.blade.php
@@ -19,20 +19,9 @@
     </div>
     <div class="page-section"> 
       <div class="container">
-        <div class="button-group filters-button-group">
-              <a href="#tag:all" for="reset">Show All</a>
-          @php
-            foreach($categories as $category) {
-              echo '<a href="#tag:'. $category->slug .'" class="project-filter project-filter__'. $category->slug .'">' . $category->name . '</a>';
-            }
-          @endphp
-        </div>
         <div class="row grid">
-          <a class="projects__type--target" id="tag:arvr"></a>
-          <a class="projects__type--target" id="tag:art-work"></a>
-          <a class="projects__type--target" id="tag:web-development"></a>
           @foreach($project_loop as $project_item)
-            <div class="col-md-6 = project__item" data-tag="{{ $project_item->project_type }}">
+            <div class="col-md-3 project__item" data-tag="{{ $project_item->project_type }}">
               <a class="projects__link" href="{{ $project_item->permalink }}">
                 <div class="projects__cards" style="background-image: url({{ $project_item->project_image['project_media']['project_images'][0]['project_image']['sizes']['large'] ? $project_item->project_image['project_media']['project_images'][0]['project_image']['sizes']['large'] : 'https://via.placeholder.com/300' }});">
                   <span class="projects__title">{{ $project_item->title }}</span>
